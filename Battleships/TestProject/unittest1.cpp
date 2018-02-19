63dcbffe1c63264ae5eb2f83ccbb2f5af00ab7ac
Karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-02-19 10:33:34 +0200

    Tests for PrintShips

--- Battleships/TestProject/unittest1.cpp
+++ Battleships/TestProject/unittest1.cpp
@@ -42,7 +42,38 @@ namespace TestProject
 			Assert::IsTrue((oss && oss.str() == "Hello\nWorld\n"));
 		}
 
-		TEST_METHOD(Gameboard_Printships_empty)
+		TEST_METHOD(Gameboard_6_Printships_empty)
+		{
+			std::stringstream oss;
+			std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+			std::cout.rdbuf(oss.rdbuf());
+
+			Gameboard game(6);
+
+			game.printShips();
+
+			string output = oss.str();
+			//Logger::WriteMessage(output);
+			std::cout.rdbuf(p_cout_streambuf);
+
+			string expected =
+				"    1 2 3 4 5 6 \n"
+				"  ----------------\n"
+				"A |              | A\n"
+				"B |              | B\n"
+				"C |              | C\n"
+				"D |              | D\n"
+				"E |              | E\n"
+				"F |              | F\n"
+				"  ----------------\n"
+				"    1 2 3 4 5 6 \n";
+
+
+
+			Assert::IsTrue((oss && oss.str() == expected));
+		}
+
+		TEST_METHOD(Gameboard_7_Printships_empty)
 		{
 			std::stringstream oss;
 			std::streambuf* p_cout_streambuf = std::cout.rdbuf();
@@ -69,12 +100,72 @@ namespace TestProject
 				"  ------------------\n"
 				"    1 2 3 4 5 6 7 \n";
 
+			Assert::IsTrue((oss && oss.str() == expected));
+		}
 
+		TEST_METHOD(Gameboard_8_Printships_empty)
+		{
+			std::stringstream oss;
+			std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+			std::cout.rdbuf(oss.rdbuf());
+
+			Gameboard game(8);
+
+			game.printShips();
+
+			string output = oss.str();
+			//Logger::WriteMessage(output);
+			std::cout.rdbuf(p_cout_streambuf);
+
+			string expected =
+				"    1 2 3 4 5 6 7 8 \n"
+				"  --------------------\n"
+				"A |                  | A\n"
+				"B |                  | B\n"
+				"C |                  | C\n"
+				"D |                  | D\n"
+				"E |                  | E\n"
+				"F |                  | F\n"
+				"G |                  | G\n"
+				"H |                  | H\n"
+				"  --------------------\n"
+				"    1 2 3 4 5 6 7 8 \n";
 
 			Assert::IsTrue((oss && oss.str() == expected));
 		}
 
-		TEST_METHOD(Gameboard_Printships_one_ship)
+
+		TEST_METHOD(Gameboard_6_Printships_one_ship)
+		{
+			std::stringstream oss;
+			std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+			std::cout.rdbuf(oss.rdbuf());
+
+			Gameboard game(6);
+
+			game.addShip(5, 1, "A1", "e");
+			game.printShips();
+
+			string output = oss.str();
+			//Logger::WriteMessage(output);
+			std::cout.rdbuf(p_cout_streambuf);
+
+			string expected =
+				"    1 2 3 4 5 6 \n"
+				"  ----------------\n"
+				"A | 5            | A\n"
+				"B | 5            | B\n"
+				"C | 5            | C\n"
+				"D | 5            | D\n"
+				"E | 5            | E\n"
+				"F |              | F\n"
+				"  ----------------\n"
+				"    1 2 3 4 5 6 \n";
+
+			Assert::IsTrue((oss && oss.str() == expected));
+		}
+
+		TEST_METHOD(Gameboard_7_Printships_one_ship)
 		{
 			std::stringstream oss;
 			std::streambuf* p_cout_streambuf = std::cout.rdbuf();
@@ -102,7 +193,37 @@ namespace TestProject
 				"  ------------------\n"
 				"    1 2 3 4 5 6 7 \n";
 
+			Assert::IsTrue((oss && oss.str() == expected));
+		}
 
+		TEST_METHOD(Gameboard_8_Printships_one_ship)
+		{
+			std::stringstream oss;
+			std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+			std::cout.rdbuf(oss.rdbuf());
+
+			Gameboard game(8);
+
+			game.addShip(5, 1, "A1", "e");
+			game.printShips();
+
+			string output = oss.str();
+			//Logger::WriteMessage(output);
+			std::cout.rdbuf(p_cout_streambuf);
+
+			string expected =
+				"    1 2 3 4 5 6 7 8 \n"
+				"  --------------------\n"
+				"A | 5                | A\n"
+				"B | 5                | B\n"
+				"C | 5                | C\n"
+				"D | 5                | D\n"
+				"E | 5                | E\n"
+				"F |                  | F\n"
+				"G |                  | G\n"
+				"H |                  | H\n"
+				"  --------------------\n"
+				"    1 2 3 4 5 6 7 8 \n";
 
 			Assert::IsTrue((oss && oss.str() == expected));
 		}
