dd6a753d5691cb78e3015824baa32fec7ec8b146
Karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-02-23 15:45:08 +0200

    Ship copy constructor, Ship hit()

--- Battleships/Battleships/Ship.cpp
+++ Battleships/Battleships/Ship.cpp
@@ -10,6 +10,10 @@ Ship::Ship(int size) :size_(size), hits_(0)
 {
 }
 
+Ship::Ship(const Ship & init): size_(init.size_), hits_(init.hits_)
+{
+}
+
 
 void Ship::setSize(int size)
 {
@@ -19,6 +23,16 @@ void Ship::setSize(int size)
 int Ship::getSize() const
 {
 	return size_;
+}
+
+int Ship::getHits() const
+{
+	return hits_;
+}
+
+void Ship::hit()
+{
+	hits_++;
 }
 
 Ship::~Ship()
--- Battleships/Battleships/Ship.h
+++ Battleships/Battleships/Ship.h
@@ -10,9 +10,13 @@ class Ship
 public:
 	Ship();
 	Ship(int size);
+	Ship(const Ship & init);
 
 	void setSize(int size);
 	int getSize() const;
+	int getHits() const;
+
+	void hit();
 
 	~Ship();
 
