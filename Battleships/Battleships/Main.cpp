f4242d59cac26e1944d252542f2c3e128d03087c
Karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-04-25 11:34:45 +0300

    readSettings fix adding ships longer than 10

--- Battleships/Battleships/Main.cpp
+++ Battleships/Battleships/Main.cpp
@@ -20,36 +20,29 @@ bool readSettings(string settingsFile, int& boardsize, int*& ships, int& longest
 
 int main()
 {
+	int boardsize;
+	int* ships;
+	int longestShip = 0;
 
-		int boardsize;
-		int* ships;
-		int longestShip = 0;
-
-		//if config file is not found or is broken, start game with default settings
-		if (!readSettings("laiva.txt", boardsize, ships, longestShip))
+	//if config file is not found or is broken, start game with default settings
+	if (!readSettings("laiva.txt", boardsize, ships, longestShip))
+	{
+		Game testgame;
+		testgame.menu(); // begin game
+	}
+	else // readSettings was succesful in getting configurations
+	{
+		int shipCount = 0;
+		for (int i = 0; i < longestShip - 1; i++)
 		{
-			Game testgame;
-			testgame.menu(); // begin game
+			shipCount += ships[i];
 		}
-		else // readSettings was succesful in getting configurations
-		{
-			int shipCount = 0;
-			for (int i = 0; i < longestShip - 1; i++)
-			{
-				//	cout << ships[i] << endl;
-				shipCount += ships[i];
-			}
 
-			//cout << "ships: " << shipCount;
+		Game testgame(boardsize, ships, longestShip - 1, shipCount);
 
-			Game testgame(boardsize, ships, longestShip - 1, shipCount);
-
-			delete[] ships; // array that was used for reading config file is no longer needed
-			testgame.menu(); // begin game
-		}
-	
-	
-	system("pause");
+		delete[] ships; // array that was used for reading config file is no longer needed
+		testgame.menu(); // begin game
+	}
 }
 
 
@@ -95,29 +88,54 @@ bool readSettings(string settingsFile, int& boardsize, int*& ships, int& longest
 					catch (const std::invalid_argument&)
 					{
 						if (flag == 1) { delete[] ships; }//delete possible dynamic memory before returning 
+						file.close();
 						return false;
 					}
 				}
 
-				else if (contents[0] == "laivat:" )
+				else if (contents[0] == "laivat:")
 				{
+					int sizes[20] = { 0 };
+					int amounts[20] = { 0 };
+
 					//find the longest ship listed in file
 					//in case user has not given them in order
 					int longest_ship = 0;
-					for (int j = 1; j < count; j++)
+					size_t test = 0;
+
+					for (int j = 0; j < count - 1; j++)
 					{
-						int size = contents[j].at(0) - '0';
-						if ((size) > longest_ship)
+						string pair = contents[j + 1];
+						test = pair.find(':');
+						int len = 0;
+						int num = 0;
+
+						try
 						{
-							longest_ship = size;
+							//Get the shiplength and number from the pair eg. 12:3
+							len = stoi(pair.substr(0, test));
+							num = stoi(pair.substr(test + 1));
+						}
+						catch (const std::exception&)
+						{
+							if (flag == 1) { delete[] ships; }
+							file.close();
+							return false;
+						}
+
+
+						sizes[j] = len;
+						amounts[j] = num;
+
+						if ((sizes[j]) > longest_ship)
+						{
+							longest_ship = sizes[j];
 						}
 					}
 					//use longest ship information to create correct size array of ship sizes
 					//if input ships are 5:2 4:1 2:3
 					//output array will be {2,1,0,3}
 					//this array is later used for gameboard constructor
-
-
 
 					int arraySize = longest_ship - 1;
 					ships = new int[arraySize];
@@ -127,18 +145,18 @@ bool readSettings(string settingsFile, int& boardsize, int*& ships, int& longest
 						ships[j] = 0;
 					}
 
-					for (int i = 1; i < count; i++)
+					for (int i = 0; i < count - 1; i++)
 					{
 						//ships[] array has shipcounts with longest ship in game at index 0.
 						//to get index of current pair (eg. 5:2)
-						int shipIndex = longest_ship - (contents[i].at(0) - '0');
-						int shipCount = contents[i].at(2) - '0';
+						int shipIndex = longest_ship - sizes[i];
+						int shipCount = amounts[i];
 						ships[shipIndex] = shipCount;
 					}
-					
+
 					longest = longest_ship;
 
-				}	
+				}
 			}
 
 			//assume that the file is bad if any exceptions are thrown
@@ -147,6 +165,7 @@ bool readSettings(string settingsFile, int& boardsize, int*& ships, int& longest
 				cout << "Virhe asetustiedostossa." << endl;
 
 				if (flag == 1) { delete[] ships; }
+				file.close();
 				return false;
 			}
 		}
@@ -156,7 +175,7 @@ bool readSettings(string settingsFile, int& boardsize, int*& ships, int& longest
 	if (longest == 0 || boardsize == 0)
 	{
 		if (flag == 1) { delete[] ships; }
-		
+		file.close();
 		return false;
 	}
 	file.close();
